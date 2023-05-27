38b7292459302ba2581793d8dcce6a89181df2ab
ameerghazal <ameerghazal@uflip.com>
2023-05-27 18:39:00 -0500

    Starting

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Build-Responsive-Real-World-Websites-with-HTML-and-CSS
+# Build-Responsive-Real-World-Websites-with-HTML-and-CSS: Section 1
+
+Section 1 of the HTML & CSS Course. Starter files, introduction into web development, webpages, and more.
---
+++ theory.js
@@ -0,0 +1,35 @@
+/*
+
+    SECTION 1: Introduction 
+
+    First Webpage 
+    - Exclamation mark with a tab creates a skeleton website structure.
+    - h1 stands for heading 1, h2 smaller, ..., hn.
+    - p stands for paragraph.
+    - Do the edits in the body element.
+    - Use the greater than less than symbols: <p>Data and close it with the right.</p>
+    
+
+
+    SECTION 2: HTML Fundamentals
+
+    Introduction to HTML
+    - HTML: Hyper Text Markup Langauge
+    - Used to describe something.
+    - Consists of elements that describe things: paragrpahs, links, headings, etc.
+    - HTML element is made of three things:
+      - Opening tag: name of the element wrapped in <> (<p>)
+      - Content: content of the element. It can contain an element in a element (for example, a div)
+      - Closing tag: same as the opening tag but with a / (</p>)
+      - For elements, such as an img element, they do not contain content, and we can omit the closing tag.
+
+  
+      HTML Document Structure
+      - 
+      -
+      -
+      -
+      -
+
+
+*/
