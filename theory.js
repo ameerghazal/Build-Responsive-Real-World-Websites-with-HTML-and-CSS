d59a8deceeac1614d976174f914006e452db7f9b
ameerghazal <ameerghazal@uflip.com>
2023-05-27 19:16:10 -0500

    couple videos in

--- theory.js
+++ theory.js
@@ -25,11 +25,24 @@
 
   
       HTML Document Structure
-      - 
-      -
-      -
-      -
-      -
+      - <!DOCTYPE html> tells the browser that the doc uses html. This is the first thing we must declare.
+      - This is followed by <html></html> an html element
+      - From there, imbedded into the html tag is a head element and a body element.
+      - <head></head> contains things that are not visible in the browser. For example, link to style sheets, page title, etc.
+      - <body></body> contains things that are always visible on the page. For example, paragraph, h1, etc.
+      - indentation is important for properly reading html.
+      - We alawys need to start with the doctype, followed by the html element, which includes the head and body tags.
+
+      Text Elements
+      - We can go all the way from h1 to h6. They all stand for heading. Lots of times, only one h1 is used.
+      - All headings use the same element structure <hn></hn> where n ranges from 1 to 6. We can use multiple heading of the same size, but h1 should only be used once.
+      - Comments use a <! -- ... --> structure, but simply ctrl slash does the job as well.
+      - Paragraph element is the <p> element, which typically uses words.
+      - Wrapping words in a <b></b> element bolds words. However, b is an old html elemnt. Instead, use the strong element, which makes it an important element to stand out: <strong> ... </strong>
+      - <strong></strong> element bolds words. 
+      - Wrapping words in a <i></i> element italizes words. However, this is outdated. Now, we should use the em element, which empasizes text.
+      - <em> ... </em> element italizes words.
+
 
 
 */
